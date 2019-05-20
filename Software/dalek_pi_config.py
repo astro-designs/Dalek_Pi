3e37e4e24ef25dc14aec77c215593cf9a95542a8
Mark Cantrill <mark@astro-designs.com>
2019-05-20 08:28:02 +0100

    Adding missing config file

---
+++ Software/dalek_pi_config.py
@@ -0,0 +1,43 @@
+# Initialisation
+# --------------
+
+# Enable / disable initial tweet at start-up
+TweetStart = True
+
+# Enable / disable final tweet at (controlled) shutdown
+TweetStop = True
+
+# Enable / disable debug functions
+DebugOn = True
+
+# Enable / disable camera functions
+CameraOn = True
+
+# Enable / disable motor control functions
+DriveOn = True
+
+# Enable / disable remote control option
+RemoteControl = True
+
+# Start message
+# -------------
+#StartTweet = 'Happy 6th Birthday @Raspberry_Pi Dalek_Pi is now on-line, LIVE at the @CotswoldJam. Send tweet_help for instructions ' + i.strftime('%Y/%m/%d %H:%M:%S') 
+#StartTweet = 'Dalek_Pi (v2.01.06) is now on-line. Send tweet_help for instructions ' + i.strftime('%Y/%m/%d %H:%M:%S') 
+#StartTweet = 'Dalek_Pi (v2.01.07) is now on-line, LIVE in the Workshop at Wychwood 2018! Send tweet_help for instructions ' + i.strftime('%Y/%m/%d %H:%M:%S')
+#StartTweet = 'Dalek_Pi (v2.01.08) is now on-line, LIVE at #RaspberryFields ! Send tweet_help for instructions ' + i.strftime('%Y/%m/%d %H:%M:%S')
+#StartTweet = 'Dalek_Pi (v2.01.08) is now on-line, LIVE at the @CotswoldJam ! Send tweet_help for instructions ' + i.strftime('%Y/%m/%d %H:%M:%S')
+#StartTweet = 'Dalek_Pi is now on-line Testing Testing Obey Obey Obey! '
+
+# Stop Message
+# ------------
+StopTweet = 'Dalek_Pi is going off-line ' 
+
+# YouTube Channel
+# ---------------
+# YouTubeCh = 'TBD'
+
+# Test Pattern Image
+
+
+
+
